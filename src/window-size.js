244dceb9d5004d4c98a3518d0cd1be5e9606e262
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-05-22 19:42:15 +0200

    Demo code

--- src/window-size.js
+++ src/window-size.js
@@ -57,4 +57,54 @@ class WindowSize extends Component {
   }
 }
 
-export default WindowSize;
+// export default WindowSize;
+
+function useWindowSize() {
+  const [height, setHeight] = React.useState(400);
+  const [width, setWidth] = React.useState(800);
+
+  React.useEffect(() => {
+    const getWindowSize = () => {
+      setHeight(window.outerHeight);
+      setWidth(window.outerWidth);
+    };
+
+    window.addEventListener("resize", getWindowSize);
+    getWindowSize();
+
+    return () => window.removeEventListener("resize", getWindowSize);
+  }, []);
+
+  return { height, width, setHeight, setWidth };
+}
+
+export default function HookedWindowSize() {
+  const { height, width, setHeight, setWidth } = useWindowSize();
+
+  return (
+    <div>
+      <h1>Window Size</h1>
+      <div>
+        <label>Height:</label>
+        <input
+          type="number"
+          value={height}
+          onChange={e => setHeight(e.target.value)}
+          autoFocus
+        />
+      </div>
+      <div>
+        <label>Width:</label>
+        <input
+          type="number"
+          value={width}
+          onChange={e => setWidth(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Area:</label>
+        <input disabled value={numberFormatter.format(height * width)} />
+      </div>
+    </div>
+  );
+}
